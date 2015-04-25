78074561c3ee19d39686522dd1a9ed89a7849595
Vish Murden <vishm_0@yahoo.co.uk>
2015-04-26 00:29:59 +0100

    implementation of loadData

---
+++ run_analysis.R
@@ -0,0 +1,46 @@
+library(dplyr)
+
+loadData <- function(folder, datasettype) {    
+    data_folder <- sprintf("%s/%s", folder, datasettype)
+    
+    
+    # Load the tests Subject ID, where each data row relates to an observation 
+    # per individual
+    subjectfilename <- sprintf("%s/subject_%s.txt",data_folder, datasettype)
+    data <- read.table(subjectfilename, header=FALSE,  colClasses="factor", 
+                       col.names=c("subject"))
+    subject<- tbl_df(data)    
+    
+    # Load the data captured form multiple individuals across many exercises
+    xdatafilename <- sprintf("%s/X_%s.txt",data_folder, datasettype)
+    x_train <- tbl_df(read.table(xdatafilename))     
+    
+    # by default read.table will read content as factor, hence need to force
+    # to character read
+    features_filename <- sprintf("%s/features.txt",folder)
+    column_names <- tolower(read.table(features_filename, colClasses="character", 
+                                header=FALSE)$V2)
+
+    
+    # find the columns that contain mean or standard deviation data
+    # and filter the X_ dataset removing all other measurements
+    valid_cols <- grepl( "mean\\(\\)", column_names) | grepl( "std\\(\\)", column_names)
+    index <- 1:length(column_names)
+    x_train <- select(x_train, index[valid_cols])
+    colnames(x_train) <- column_names[valid_cols]
+    
+    
+    activity_filename <- sprintf("%s/activity_labels.txt",folder)
+    actvity_names <- tbl_df(read.csv(activity_filename, header = FALSE, sep = " "))
+    
+    # Load ID of acivity being undertaken          
+    ydatafilename <- sprintf("%s/y_%s.txt",data_folder, datasettype)
+    y_train <- tbl_df(read.table(ydatafilename))     
+    y_train <- left_join(y_train, actvity_names, by = "V1")
+    y_train <- rename(y_train, activity = V2)
+    
+    
+    # join/merge all the dataset    
+    all <- bind_cols(subject, x_train) %>% bind_cols(select(y_train, activity))
+    all
+}
